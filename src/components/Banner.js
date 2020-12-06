e6df8204c72ac0e53c44e0b4636634cd9115a29e
bmonish <monishmonib@gmail.com>
2020-12-06 23:26:36 +0530

    Temporary Banner and Fonts

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "../css/Banner.css";
 
 function Banner() {
-  return <div className="banner"></div>;
+  return (
+    <div className="banner">
+      <h2>I am Banner</h2>
+    </div>
+  );
 }
 
 export default Banner;
