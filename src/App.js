c6c7c4049da1c1306e734304cbaafa6010b4aae7
bmonish <monishmonib@gmail.com>
2020-12-14 19:32:18 +0530

    Uploaded Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header";
+import Banner from "./components/Banner";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Banner />
     </div>
   );
 }
