46fd67f8ac3e640e2184774b84f14f6eb42b7d36
bmonish <monishmonib@gmail.com>
2021-01-11 02:02:10 +0530

    HREF error

--- src/components/About.js
+++ src/components/About.js
@@ -3,26 +3,24 @@ import "../css/About.css";
 
 function About() {
   return (
-    <a href="#home" name="about">
-      <div className="about">
-        <div className="about__left">
-          <p className="about__accent">About Me</p>
-          <h1>I am a Web Developer, based in Chennai</h1>
-          <p className="about__para">
-            Student by Profession. Developer by Heart &hearts; <br></br>I'm a
-            College Graduate aspiring to become a Full-Stack Developer.
-          </p>
-        </div>
-        <div className="about__right">
-          <p className="about__accent">Contact</p>
-          <h1>Have any query or question?</h1>
-          <p className="about__para">Let's Talk</p>
-          <h2 className="about__accent email">
-            <a href="mailto:monishmonib@gmail.com">{`monishmonib@gmail.com ->`}</a>
-          </h2>
-        </div>
+    <div className="about">
+      <div className="about__left">
+        <p className="about__accent">About Me</p>
+        <h1>I am a Web Developer, based in Chennai</h1>
+        <p className="about__para">
+          Student by Profession. Developer by Heart &hearts; <br></br>I'm a
+          College Graduate aspiring to become a Full-Stack Developer.
+        </p>
       </div>
-    </a>
+      <div className="about__right">
+        <p className="about__accent">Contact</p>
+        <h1>Have any query or question?</h1>
+        <p className="about__para">Let's Talk</p>
+        <h2 className="about__accent email">
+          <a href="mailto:monishmonib@gmail.com">{`monishmonib@gmail.com ->`}</a>
+        </h2>
+      </div>
+    </div>
   );
 }
 
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,36 +3,34 @@ import "../css/Contact.css";
 
 function Contact() {
   return (
-    <a href="#home" name="contact">
-      <div className="contact">
-        <div className="contact__left">
-          <p className="contact__header">CONNECT</p>
-          <h1>
-            Got a Idea?<br></br>Let's Talk
-          </h1>
-          <h2 className="about__accent email">
-            <a href="mailto:monishmonib@gmail.com">{`monishmonib@gmail.com ->`}</a>
-          </h2>
-        </div>
-        <div className="contact__right">
-          <p className="contact__header">
-            ESTIMATE YOUR PROJECT?<br></br>Let me know here
-          </p>
-          <form method="POST" netlify>
-            <h2>Whats your name?</h2>
-            <input type="text" name="name" />
-            <h2>Whats your email?</h2>
-            <input type="text" name="email" />
-            <h2>Tell me about your project?</h2>
-            <input type="text" name="project" />
-            <br></br>
-            <button className="submit__button" type="submit">
-              Submit
-            </button>
-          </form>
-        </div>
+    <div className="contact">
+      <div className="contact__left">
+        <p className="contact__header">CONNECT</p>
+        <h1>
+          Got a Idea?<br></br>Let's Talk
+        </h1>
+        <h2 className="about__accent email">
+          <a href="mailto:monishmonib@gmail.com">{`monishmonib@gmail.com ->`}</a>
+        </h2>
       </div>
-    </a>
+      <div className="contact__right">
+        <p className="contact__header">
+          ESTIMATE YOUR PROJECT?<br></br>Let me know here
+        </p>
+        <form name="contactForm" netlify>
+          <h2>Whats your name?</h2>
+          <input type="text" name="name" />
+          <h2>Whats your email?</h2>
+          <input type="text" name="email" />
+          <h2>Tell me about your project?</h2>
+          <input type="text" name="project" />
+          <br></br>
+          <button className="submit__button" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
