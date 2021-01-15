1e8026e388e51e31f375bf4e40933012a618d667
bmonish <monishmonib@gmail.com>
2021-01-15 18:38:25 +0530

    Banner and Skills

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import "./App.css";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
-import About from "./components/About";
 import Specific from "./components/Specific";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
