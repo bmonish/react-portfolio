dcc04eb0a9bb374d6629dac2f778c94919f13447
bmonish <monishmonib@gmail.com>
2021-01-16 23:41:39 +0530

    About Styling

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,20 @@ function About() {
   return (
     <div className="about">
       <div className="about__left">
-        <p class="about__accent">About Me</p>
+        <p className="about__accent">About Me</p>
         <h1>I am a Web Developer, based in Chennai</h1>
+        <p className="about__para">
+          Student by Profession. Developer by Heart &hearts; <br></br>I'm a
+          College Graduate aspiring to become a Full-Stack Developer.
+        </p>
       </div>
       <div className="about__right">
-        <p class="about__accent">Contact</p>
+        <p className="about__accent">Contact</p>
         <h1>Have any query or question?</h1>
-        <p>Let's Talk</p>
-        <h2 class="about__accent">{`monishmonib@gmail.com`}</h2>
+        <p className="about__para">Let's Talk</p>
+        <h2 className="about__accent email">
+          <a href="mailto:monishmonib@gmail.com">monishmonib@gmail.com -></a>
+        </h2>
       </div>
     </div>
   );
